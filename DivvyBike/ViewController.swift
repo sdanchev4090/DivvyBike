c2abe7cb2fb0601b0c4ef3bf46afbe031977b641
Sava Danchev <sdanchev4090@stu.d214.org>
2022-12-16 10:35:44 -0600

    1st

--- DivvyBike/ViewController.swift
+++ DivvyBike/ViewController.swift
@@ -8,12 +8,22 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var array : [String] = []
+    
+    let url = URL(string: "https://data.cityofchicago.org/resource/aavc-b2wj.json")
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        getData()
+    }
+    
+    func getData() {
+        
     }
 
-
+    
+    
 }
 
